c248b168cbda84db0ca26f36d1d268294590621e
razorbladex401 <razorbladex401@gmail.com>
2020-03-02 15:24:59 -0500

    Add Microsoft Security Compliance Toolkit 1.0 templates https://www.microsoft.com/en-us/download/details.aspx?id=55319

--- manifests/init.pp
+++ manifests/init.pp
@@ -7,4 +7,53 @@ class advanced_security_policy {
     group  => 'Administrators',
     mode   => '0770',
   }
+
+  file { 'C:/Windows/PolicyDefinitions/AdmPwd.admx':
+    ensure => file,
+    source => 'puppet:///modules/advanced_security_policy/AdmPwd.admx',
+    owner  => 'Administrators',
+    group  => 'Administrators',
+    mode   => '0770',
+  }
+
+  file { 'C:/Windows/PolicyDefinitions/en-US/AdmPwd.adml':
+    ensure => file,
+    source => 'puppet:///modules/advanced_security_policy/AdmPwd.adml',
+    owner  => 'Administrators',
+    group  => 'Administrators',
+    mode   => '0770',
+  }
+
+  file { 'C:/Windows/PolicyDefinitions/MSS-legacy.admx':
+    ensure => file,
+    source => 'puppet:///modules/advanced_security_policy/MSS-legacy.admx',
+    owner  => 'Administrators',
+    group  => 'Administrators',
+    mode   => '0770',
+  }
+
+  file { 'C:/Windows/PolicyDefinitions/en-US/MSS-legacy.adml':
+    ensure => file,
+    source => 'puppet:///modules/advanced_security_policy/MSS-legacy.adml',
+    owner  => 'Administrators',
+    group  => 'Administrators',
+    mode   => '0770',
+  }
+
+  file { 'C:/Windows/PolicyDefinitions/SecGuide.admx':
+    ensure => file,
+    source => 'puppet:///modules/advanced_security_policy/SecGuide.admx',
+    owner  => 'Administrators',
+    group  => 'Administrators',
+    mode   => '0770',
+  }
+
+  file { 'C:/Windows/PolicyDefinitions/en-US/SecGuide.adml':
+    ensure => file,
+    source => 'puppet:///modules/advanced_security_policy/SecGuide.adml',
+    owner  => 'Administrators',
+    group  => 'Administrators',
+    mode   => '0770',
+  }  
+
 }
